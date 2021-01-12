63743a5919294609531c8cde56f7c199aac40593
jslee6091 <jslee6091@naver.com>
2021-01-12 18:24:14 +0900

    django practice

--- matplotlib_examples/group_bar_plot.py
+++ matplotlib_examples/group_bar_plot.py
@@ -1,25 +1,25 @@
-#Group Bar Plot In MatPlotLib
+# Group Bar Plot In MatPlotLib
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
 raw_data = {'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
-        'pre_score': [4, 24, 31, 2, 3],
-        'mid_score': [25, 94, 57, 62, 70],
-        'post_score': [5, 43, 23, 23, 51]}
-df = pd.DataFrame(raw_data, columns = ['first_name', 'pre_score', 'mid_score', 'post_score'])
+            'pre_score': [4, 24, 31, 2, 3],
+            'mid_score': [25, 94, 57, 62, 70],
+            'post_score': [5, 43, 23, 23, 51]}
+df = pd.DataFrame(raw_data, columns=['first_name', 'pre_score', 'mid_score', 'post_score'])
 
 # Setting the positions and width for the bars
 pos = list(range(len(df['pre_score'])))
 width = 0.25
 
 # Plotting the bars
-fig, ax = plt.subplots(figsize=(10,5))
+fig, ax = plt.subplots(figsize=(10, 5))
 
 # Create a bar with pre_score data,
 # in position pos,
 plt.bar(pos,
-        #using df['pre_score'] data,
+        # using df['pre_score'] data,
         df['pre_score'],
         # of width
         width,
@@ -33,7 +33,7 @@ plt.bar(pos,
 # Create a bar with mid_score data,
 # in position pos + some width buffer,
 plt.bar([p + width for p in pos],
-        #using df['mid_score'] data,
+        # using df['mid_score'] data,
         df['mid_score'],
         # of width
         width,
@@ -46,8 +46,8 @@ plt.bar([p + width for p in pos],
 
 # Create a bar with post_score data,
 # in position pos + some width buffer,
-plt.bar([p + width*2 for p in pos],
-        #using df['post_score'] data,
+plt.bar([p + width * 2 for p in pos],
+        # using df['post_score'] data,
         df['post_score'],
         # of width
         width,
@@ -71,8 +71,8 @@ ax.set_xticks([p + 1.5 * width for p in pos])
 ax.set_xticklabels(df['first_name'])
 
 # Setting the x-axis and y-axis limits
-plt.xlim(min(pos)-width, max(pos)+width*4)
-plt.ylim([0, max(df['pre_score'] + df['mid_score'] + df['post_score'])] )
+plt.xlim(min(pos) - width, max(pos) + width * 4)
+plt.ylim([0, max(df['pre_score'] + df['mid_score'] + df['post_score'])])
 
 # Adding the legend and showing the plot
 plt.legend(['Pre Score', 'Mid Score', 'Post Score'], loc='upper left')
--- matplotlib_examples/pie_plot.py
+++ matplotlib_examples/pie_plot.py
@@ -1,13 +1,13 @@
-#%matplotlib inline
+# %matplotlib inline
 import pandas as pd
 import matplotlib.pyplot as plt
 
 raw_data = {'officer_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
-        'jan_arrests': [4, 24, 31, 2, 3],
-        'feb_arrests': [25, 94, 57, 62, 70],
-        'march_arrests': [5, 43, 23, 23, 51]}
+            'jan_arrests': [4, 24, 31, 2, 3],
+            'feb_arrests': [25, 94, 57, 62, 70],
+            'march_arrests': [5, 43, 23, 23, 51]}
 
-df = pd.DataFrame(raw_data, columns = ['officer_name', 'jan_arrests', 'feb_arrests', 'march_arrests'])
+df = pd.DataFrame(raw_data, columns=['officer_name', 'jan_arrests', 'feb_arrests', 'march_arrests'])
 df['total_arrests'] = df['jan_arrests'] + df['feb_arrests'] + df['march_arrests']
 print(df['total_arrests'])
 # Create a list of colors (from iWantHue)
@@ -29,11 +29,11 @@ plt.pie(
     startangle=90,
     # with the percent listed as a fraction
     autopct='%1.1f%%',
-    )
+)
 
 # View the plot drop above
 plt.axis('equal')
 
 # View the plot
-#plt.tight_layout()
+# plt.tight_layout()
 plt.show()
